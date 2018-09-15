b4381b2754401ab39124fd1466f734c90179b08b
Curly Joe <curlyjoe71@gmail.com>
2018-09-15 10:09:22 -0700

    Updated working files.

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -8,32 +8,49 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var displayName, email, emailVerified, photoURL, isAnonymous, uid, providerData, avatarURL, div, images, avatarURL, avatar;
-
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        // User is signed in.
-        displayName = user.displayName;
-        email = user.email;
-        emailVerified = user.emailVerified;
-        photoURL = user.photoURL;
-        isAnonymous = user.isAnonymous;
-        uid = user.uid;
-        providerData = user.providerData;
-        // ...
-        console.log(displayName, email, emailVerified, photoURL, isAnonymous, uid, providerData);
-        var playerOne = $('<span>Hello, ' + displayName + '! You\'re Player One!</span>');
-        $('#playerOneDisplay').append(playerOne);
-    } else {
-        // User is signed out.
-        // ...
-    }
-});
-
+var displayName, email, emailVerified, photoURL, isAnonymous, uid, providerData, avatarURL, div, images, avatarURL, avatar, playerOne, playerTwo;
+var database = firebase.database();
 var user = firebase.auth().currentUser;
 
+// firebase.auth().onAuthStateChanged(function (user) {
+//     if (user) {
+//         // User is signed in.
+//         displayName = user.displayName;
+//         email = user.email;
+//         emailVerified = user.emailVerified;
+//         photoURL = user.photoURL;
+//         isAnonymous = user.isAnonymous;
+//         uid = user.uid;
+//         providerData = user.providerData;
+//         // ...
+//         console.log(displayName, email, emailVerified, photoURL, isAnonymous, uid, providerData);
 
-var photoArray = ['https://vignette.wikia.nocookie.net/simpsons/images/f/fb/Bart%27s_youngest_son.png/revision/latest?cb=20141028004843', 'https://vignette.wikia.nocookie.net/simpsons/images/5/57/Lisa_Simpson2.png/revision/latest?cb=20180319000458', 'https://vignette.wikia.nocookie.net/simpsons/images/8/89/Maggie.png/revision/latest?cb=20090115172358', 'https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20180319061933', 'https://vignette.wikia.nocookie.net/simpsons/images/4/4d/MargeSimpson.png/revision/latest?cb=20180314071936', 'https://vignette.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png/revision/latest?cb=20091207194310'];
+//     } 
+//     else {
+//         // User is signed out.
+//         // ...
+//     }
+// });
+
+var updateDatabase = () => {
+    console.log('user is: ' + user);
+    console.log('displayname is: ' + displayName);
+
+    if (firebase.database().ref('connections')) {
+        firebase.database().ref('playertwo').set({
+            playerName: displayName,
+            avatar: photoURL
+        })
+    }
+    else {
+        firebase.database().ref('playerone').set({
+            playerName: displayName,
+            avatar: photoURL
+        });
+    }
+};
+
+var photoArray = ['https://vignette.wikia.nocookie.net/simpsons/images/5/57/Lisa_Simpson2.png/revision/latest?cb=20180319000458', 'https://vignette.wikia.nocookie.net/simpsons/images/8/89/Maggie.png/revision/latest?cb=20090115172358', 'https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20180319061933', 'https://vignette.wikia.nocookie.net/simpsons/images/4/4d/MargeSimpson.png/revision/latest?cb=20180314071936', 'https://vignette.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png/revision/latest?cb=20091207194310'];
 
 var renderAvatars = () => {
     for (i = 0; i < photoArray.length; i++) {
@@ -47,15 +64,15 @@ var renderAvatars = () => {
     };
 };
 
-var database = firebase.database();
 
+//***CONNECTION CODE***//
 var connectionsRef = database.ref('/connections');
 console.log(connectionsRef);
 
 var connectedRef = database.ref('.info/connected');
 
 connectedRef.on('value', function (snap) {
-    if (snap.numChildren === 2) {
+    if (connectionsRef.length === 2) {
         alert("There are already two players playing. Try again later.");
     }
 
@@ -65,6 +82,7 @@ connectedRef.on('value', function (snap) {
         con.onDisconnect().remove();
     }
 });
+//***END OF CONNECTION CODE***/
 
 var updatePic = () => {
     var user = firebase.auth().currentUser;
@@ -76,7 +94,7 @@ var updatePic = () => {
     }).catch(function (error) {
         // An error happened.
     });
-    console.log(user);    
+    console.log(user);
 }
 
 var updatePhoto = () => {
@@ -84,6 +102,29 @@ var updatePhoto = () => {
     console.log(avatarURL);
     updatePic();
 };
+
+firebase.database().ref().on('value', function(snap){
+    console.log(snap.val());
+
+    if(snap.child('playerone').exists()) {
+        console.log('playerone exists');
+        firebase.database().ref('playertwo').set({
+            playerName: displayName,
+            avatar: photoURL
+        });
+        playerTwo = $('<span>Hello, ' + displayName + '! You\'re Player Two!</span>');
+        $('#playerTwoDisplay').append(playerTwo);
+    }
+    else {
+        firebase.database().ref('playerone').set({
+            playerName: displayName,
+            avatar: photoURL
+        });
+        playerOne = $('<span>Hello, ' + displayName + '! You\'re Player One!</span>');
+        $('#playerOneDisplay').append(playerOne);
+    }
+});
+
 
 $(document).ready(function () {
     renderAvatars();
@@ -93,3 +134,12 @@ $(document).ready(function () {
         updatePhoto();
     }); //end of click event
 });
+
+//I need to take the curent user data and just move it over to the database.
+
+//I ended up getting rid of the auth part of the app.
+
+
+// If there isn't anything at child index 0, then all of this is player one information.
+//The next person to logs in, but if there is already a spot at index 0, then get pushed to index 1, and then their information is player two information.
+// 
